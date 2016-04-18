0c47550c19cbea9e22e90772c74bc62f0897380d
Joseph Dale <joe@Josephs-MBP.attlocal.net>
2016-04-18 14:01:41 -0500

    now using find(tr) to grab nodes

--- home.js
+++ home.js
@@ -15,13 +15,13 @@ request(url, function(error, response, html){
     var favorite, dawg, time, spread, total, odds;
     var json = {time: "", favorite: "", spread: "", dawg: "", total: "", odds: ""};
 
-    var data = $("td > b:contains('Money Odds')").first().parent().parent().next();
-    json.time = data.children("td:nth-child(1)").text();
-    json.favorite = data.children("td:nth-child(2)").text();
-    json.spread = data.children("td:nth-child(3)").text();
-    json.dawg = data.children("td:nth-child(4)").text();
-    json.total = data.children("td:nth-child(5)").text();
-    json.odds = data.children("td:nth-child(6)").text();
+    var data = $("b:contains('Closing Las Vegas NFL Odds From Week 1, 2006')").parent().parent().parent().parent().find("tr");
+    // json.time = data.children("td:nth-child(1)").text();
+    // json.favorite = data.children("td:nth-child(2)").text();
+    // json.spread = data.children("td:nth-child(3)").text();
+    // json.dawg = data.children("td:nth-child(4)").text();
+    // json.total = data.children("td:nth-child(5)").text();
+    // json.odds = data.children("td:nth-child(6)").text();
 
 }
 
@@ -31,7 +31,8 @@ request(url, function(error, response, html){
 // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
 // Parameter 3 :  callback function - a callback function to let us know the status of our function
 
-fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+// fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+fs.writeFile('output.json', data, function(err){
 
     console.log('File successfully written! - Check your project directory for the output.json file');
 
@@ -40,7 +41,7 @@ fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
 // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
 res.send('Check your console!')
 
-    }) ;
+    });
 })
 
 app.listen('1989')
