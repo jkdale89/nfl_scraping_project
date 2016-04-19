e7c966930508d219b80d95d953feb6acab8af2b6
Joseph Dale <joe@Josephs-MBP.attlocal.net>
2016-04-19 14:21:20 -0500

    looping through different years, but need a better json organization structure

--- home.js
+++ home.js
@@ -11,44 +11,31 @@ url = "http://www.footballlocks.com/nfl_odds_week_1.shtml";
 request(url, function(error, response, html){
     if(!error){
         var $ = cheerio.load(html);
-
-    var data = $("b:contains('Closing Las Vegas NFL Odds From Week 1, 2013')").parent().parent().parent().parent().find("tr:has(td)");
-    var result = [];
-    result.length = data.length;
-
-    for(var i = 0; i < result.length; i ++){
-      result[i] =
-      {
-        "Time" : data.eq(i).children().eq(0).text(),
-        "Favorite" : data.eq(i).children().eq(1).text(),
-        "Spread" : data.eq(i).children().eq(2).text(),
-        "Dawg" : data.eq(i).children().eq(3).text(),
-        "Total" : data.eq(i).children().eq(4).text(),
-        "Odds" : data.eq(i).children().eq(5).text()
+        for(var i = 2006; i < 2015; i++){
+          var year_str = i.toString();
+          var data = $("b:contains('Closing Las Vegas NFL Odds From Week 1, " + year_str + "')").parent().parent().parent().parent().find("tr:has(td)");
+          var result = [];
+          result.length = data.length;
+          for(var j = 0; j < 18; j ++){
+            result[j] = {
+              "Time" : data.eq(j).children().eq(0).text(),
+              "Favorite" : data.eq(j).children().eq(1).text(),
+              "Spread" : data.eq(j).children().eq(2).text(),
+              "Dawg" : data.eq(j).children().eq(3).text(),
+              "Total" : data.eq(j).children().eq(4).text(),
+              "Odds" : data.eq(j).children().eq(5).text()
+            }
+          }
+        result.shift();
+        console.log(result);
+        fs.writeFile("w1_" + year_str + ".json", JSON.stringify(result, null, 6), function(err){
+          console.log('File successfully written! - Check your project directory for the output.json file');
+        })
       }
     }
-    result.shift();
-    console.log(result);
-}
-
-// To write to the system we will use the built in 'fs' library.
-// In this example we will pass 3 parameters to the writeFile function
-// Parameter 1 :  output.json - this is what the created filename will be called
-// Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-// Parameter 3 :  callback function - a callback function to let us know the status of our function
-
-// fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-fs.writeFile('w1_2013.json', JSON.stringify(result, null, 6), function(err){
-
-    console.log('File successfully written! - Check your project directory for the output.json file');
-
-})
-
-// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-res.send(data.text())
-
-    });
-})
+  res.send(data.text())
+  })
+});
 
 app.listen('1989')
 console.log('Magic happens on port 1989');
