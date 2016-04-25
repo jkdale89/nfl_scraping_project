319777fef889caa87702d16dd912b44ccf5cf2e5
Joseph Dale <joe@Josephs-MBP.attlocal.net>
2016-04-25 17:43:57 -0500

    week and year now included in each matchup object, redundant but just in case

--- scrape.js
+++ scrape.js
@@ -5,32 +5,34 @@ var cheerio = require('cheerio');
 var app     = express();
 
 var populate = function(week){
-url = "http://www.footballlocks.com/nfl_odds_week_" + week.toString() + ".shtml";
-request(url, function(error, response, html){
-  if(!error){
-      var $ = cheerio.load(html);
-      for(var i = 2006; i <= 2015; i++){
-        var year_str = i.toString();
-        var data = $("b:contains('Closing Las Vegas NFL Odds From Week " + week.toString() + ", " + year_str + "')").parent().parent().parent().parent().find("tr:has(td)");
-        var result = [];
-        result.length = data.length;
-        for(var j = 0; j < 18; j ++){
-          result[j] = {
-            "Time" : data.eq(j).children().eq(0).text(),
-            "Favorite" : data.eq(j).children().eq(1).text(),
-            "Spread" : data.eq(j).children().eq(2).text(),
-            "Dawg" : data.eq(j).children().eq(3).text(),
-            "Total" : data.eq(j).children().eq(4).text(),
-            "Odds" : data.eq(j).children().eq(5).text()
+  url = "http://www.footballlocks.com/nfl_odds_week_" + week.toString() + ".shtml";
+  request(url, function(error, response, html){
+    if(!error){
+        var $ = cheerio.load(html);
+        for(var i = 2006; i <= 2015; i++){
+          var year_str = i.toString();
+          var data = $("b:contains('Closing Las Vegas NFL Odds From Week " + week.toString() + ", " + year_str + "')").parent().parent().parent().parent().find("tr:has(td)");
+          var result = [];
+          result.length = data.length;
+          for(var j = 0; j < 18; j ++){
+            result[j] = {
+              "Time" : data.eq(j).children().eq(0).text(),
+              "Favorite" : data.eq(j).children().eq(1).text(),
+              "Spread" : data.eq(j).children().eq(2).text(),
+              "Dawg" : data.eq(j).children().eq(3).text(),
+              "Total" : data.eq(j).children().eq(4).text(),
+              "Odds" : data.eq(j).children().eq(5).text(),
+              "Week" : week,
+              "Year" : i
+            }
           }
-        }
-      result.shift();
-      console.log(result);
-      fs.writeFile(week.toString() + "_" + year_str + ".json", JSON.stringify(result, null, 6), function(err){
-        console.log('File successfully written! - Check your project directory for the output.json file');
-      })
+        result.shift();
+        console.log(result);
+        fs.writeFile(week.toString() + "_" + year_str + ".json", JSON.stringify(result, null, 6), function(err){
+          console.log('File successfully written! - Check your project directory for the output.json file');
+        })
+      }
     }
-  }
   })
 }
 
