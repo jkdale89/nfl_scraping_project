b00bd5b7a5650f46690c93c3fe9eb9196255e438
Joseph Dale <joe@Josephs-MacBook-Pro.local>
2016-05-25 13:49:04 -0700

    more aggregation - discrepancy in titles of new york teams - creating list of id's to reconcile two data sets

--- data/aggregation/merge.js
+++ data/aggregation/merge.js
@@ -1,48 +1,32 @@
 var fs = require('fs'),
-    results = require("./results.js"),
-    len = Object.keys(results[0]).length,
-    newColl = [];
+    results = require("./js/clean_results.js"),
+    lines = require("./js/clean_lines.js"),
+    aggregate = [],
+    teamsList = [];
 
-
-
-  for(var i = 0; i < results.length; i++){
-    if(results[i]["Result"] === "Postponed"){
-      i ++
-    }
-    else {
-      var newObj = {}
-        , away = results[i]["Away Team"]
-        , awayArray = away.split(" ")
-        , home = results[i]["Home Team"]
-        , homeArray = home.split(" ")
-        , score = results[i]["Result"]|| "This is the problematic one"
-        , scoreArray = score.split(",")
-
-        // establish variables for winning team, winning score
-        , winnerArr = scoreArray[0].split(" ")
-        , winner = winnerArr[0]
-        , winningScore = winnerArr[1]
-        // establish variables for losing team, losing score
-        , loserArr = scoreArray[1].split(" ")
-        , loser = loserArr[1]
-        , losingScore = loserArr[2];
-
-        newObj.awayNickname = awayArray.pop();
-        newObj.awayTeam = awayArray.join(" ");
-        newObj.homeNickname = homeArray.pop();
-        newObj.homeTeam = homeArray.join(" ");
-        newObj.winner = winner;
-        newObj.winningScore = parseInt(winningScore);
-        newObj.loser = loser;
-        newObj.losingScore = parseInt(losingScore);
-        newObj.week = results[i]["Week"];
-        newObj.year = results[i]["Year"];
-        //This creates a semantic id in form of "homeNick/awayNick/week/year". Can be used for tying together lines
-        newObj.idj = newObj.homeNickname + newObj.awayNickname + newObj.week.toString() + newObj.year.toString();
-        newColl.push(newObj);
+    for(var i = 0; i < results.length; i ++) {
+      if(teamsList.indexOf(results[i].awayTeam)){
+        i++
       }
-  }
+      else {
+        teamsList.push(results[i].awayTeam)
+      }
+    }
+    console.log(teamsList)
 
-fs.writeFile("clean_lines.json", JSON.stringify(newColl, null, 6), function(err) {
-  console.log("check this dir for the output.")
-});
+    // for(var i = 0; i < lines.length; i ++) {
+    //   var id = lines[i].idj;
+    //   for(var j = 0; j < results.length; j ++){
+    //     if (id === results[j].idj) {
+    //       lines[i].winner = results[j].winner;
+    //       lines[i].loser = results[j].loser;
+    //       lines[i].winningScore = results[j].winningScore;
+    //       lines[i].losingScore = results[j].losingScore;
+    //     }
+    //   }
+    //   aggregate.push(lines[i]);
+    // }
+
+  fs.writeFile("aggregate.json", JSON.stringify(aggregate, null, 6), function(err) {
+      console.log("check aggreagate.json for the output.")
+  });
