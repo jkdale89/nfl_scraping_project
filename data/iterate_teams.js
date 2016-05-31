41c8be43ec6f78df46e52f79d3d9272231c65770
Joseph Dale <joe@Josephs-MacBook-Pro.local>
2016-05-31 14:54:42 -0700

    stable iterating for one team, one season

--- data/iterate_teams.js
+++ data/iterate_teams.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
     , games = require("./js/games.js")
     , no_games = games.length
-    , arr = [];
+    , arr = []
+    , master = {};
 
 
     //let's test populating one season, for one team
@@ -21,40 +22,45 @@ var fs = require('fs')
           ){
             if(arr[k-1] === undefined){
               arr[k-1] = arr[k-2];
+              // arr[k-1].spread = "Bye";
+              // arr[k-1].week = k;
+              // arr[k-1].favorite = "Bye";
             }
             var spread = games[i].spread;
             //we'll persist the data differently, depending on
             //whether our team is a favorite or underdog
             if(games[i].favorite === "Cleveland"){
-              var wins_ats = arr[k-1] ? arr[k-1].wins_ats + (1 * (games[i].favorite_diff_ats > 0)) : 1 * (games[i].favorite_diff_ats > 0)
-              , losses_ats = arr[k-1] ? arr[k-1].losses_ats + (1 * (games[i].dog_diff_ats > 0)) : 1 * (games[i].favorite_diff_ats < 0)
-              , ties_ats = arr[k-1] ? arr[k-1].ties_ats + (1 * (games[i].favorite_diff_ats === 0)) : 1 * (games[i].favorite_diff_ats === 0);
               arr[k] = {
                 favorite : true,
                 team : "Cleveland",
                 week : k,
                 year : j,
                 spread: spread,
-                result_ats: games[i].favorite_diff_ats,
-                wins_ats: wins_ats,
-                losses_ats: losses_ats,
-                ties_ats: ties_ats
+                ml: games[i].ml_fav,
+                home: games[i].home === "Cleveland",
+                winner: games[i].winner === "Cleveland",
+                winningScore: games[i].winningScore,
+                losingScore: games[i].losingScore,
+                ats: games[i].favorite_diff_ats,
+                total: games[i].total,
+                over: games[i].over
               };
             }
             if(games[i].dog === "Cleveland"){
-              var wins_ats = arr[k-1] ? arr[k-1].wins_ats + (1 * (games[i].dog_diff_ats > 0)) : 1 * (games[i].dog_diff_ats > 0)
-              , losses_ats = arr[k-1] ? arr[k-1].losses_ats + (1 * (games[i].dog_diff_ats < 0)) : 1 * (games[i].dog_diff_ats < 0)
-              , ties_ats = arr[k-1] ? arr[k-1].ties_ats + (1 * (games[i].favorite_diff_ats === 0)) : 1 * (games[i].dog_diff_ats === 0);
               arr[k] = {
                 favorite : false,
                 team : "Cleveland",
                 week : k,
                 year : j,
                 spread: spread * -1,
-                result_ats: games[i].dog_diff_ats,
-                wins_ats: wins_ats,
-                losses_ats: losses_ats,
-                ties_ats: ties_ats
+                ml: games[i].ml_dog,
+                home: games[i].home === "Cleveland",
+                winner: games[i].winner === "Cleveland",
+                winningScore: games[i].winningScore,
+                losingScore: games[i].losingScore,
+                ats: games[i].dog_diff_ats,
+                total: games[i].total,
+                over: games[i].over
               };
             }
           }
@@ -62,4 +68,35 @@ var fs = require('fs')
       }
     }
 
-console.log(arr)
+arr.shift();
+
+for(var i = 0; i < arr.length; i++){
+  // find the bye week, and don't iterate it
+  if(arr[i-1] && (arr[i].week == arr[i-1].week)){
+    arr[i].wins_ats = arr[i-2] ? arr[i-2].wins_ats + (1 * (arr[i].ats > 0)) : (1 * (arr[i].ats > 0)),
+    arr[i].losses_ats = arr[i-2] ? arr[i-2].losses_ats + (1 * (arr[i].ats < 0)) : (1 * (arr[i].ats < 0)),
+    arr[i].ties_ats = arr[i-2] ? arr[i-2].ties_ats + (1 * (arr[i].ats === 0)) : (1 * (arr[i].ats === 0)),
+    arr[i].record_ats = arr[i].wins_ats + "-" + arr[i].losses_ats + "-" + arr[i].ties_ats,
+    arr[i].wins = arr[i-2] ? arr[i-2].wins + (1 * (arr[i].winner)) : (1 * (arr[i].winner)),
+    arr[i].losses = arr[i-2] ? arr[i-2].losses + (1 * !(arr[i].winner)) : (1 * !(arr[i].winner)),
+    arr[i].record = arr[i].wins + "-" + arr[i].losses;
+  }
+  else {
+    arr[i].wins_ats = arr[i-1] ? arr[i-1].wins_ats + (1 * (arr[i].ats > 0)) : (1 * (arr[i].ats > 0)),
+    arr[i].losses_ats = arr[i-1] ? arr[i-1].losses_ats + (1 * (arr[i].ats < 0)) : (1 * (arr[i].ats < 0)),
+    arr[i].ties_ats = arr[i-1] ? arr[i-1].ties_ats + (1 * (arr[i].ats === 0)) : (1 * (arr[i].ats === 0)),
+    arr[i].record_ats = arr[i].wins_ats + "-" + arr[i].losses_ats + "-" + arr[i].ties_ats,
+    arr[i].wins = arr[i-1] ? arr[i-1].wins + (1 * (arr[i].winner)) : (1 * (arr[i].winner)),
+    arr[i].losses = arr[i-1] ? arr[i-1].losses + (1 * !(arr[i].winner)) : (1 * !(arr[i].winner)),
+    arr[i].record = arr[i].wins + "-" + arr[i].losses;
+  }
+}
+
+
+master["Cleveland"] = {};
+
+master["Cleveland"]["2006"] = arr;
+
+fs.writeFile("test.json", JSON.stringify(master, null, 6), function(err){
+  console.log("check test.json i guess?")
+});
